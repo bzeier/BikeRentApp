27851d7597a8ac61ce855c5d736c74b8983b645f
Brandon Zeier <43791891+bzeier@users.noreply.github.com>
2023-05-30 13:36:20 +0200

    add bike model

---
+++ Models/Bike.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentApp.Models
+{
+    internal class Bike
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public User Owner { get; set; }
+
+        public float PricePerHour { get; set; }
+
+        public Bike(string name, string description, User owner, float pricePerHour)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Owner = owner;
+            this.PricePerHour = pricePerHour;
+        }
+    }
+}
